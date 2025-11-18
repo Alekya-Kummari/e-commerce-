5cd9e993ffda04b85d402e58e6ec18905184427a
Alekya-Kummari <alekyakummari03@gmail.com>
2025-11-18 15:50:33 +0530

    com

--- e-commerce/ServicesPortal/ServicesPortal/settings.py
+++ e-commerce/ServicesPortal/ServicesPortal/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[Static_dir]
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'Static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
